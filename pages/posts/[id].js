2c30837e93d0cf7e3c1f7e384b8b262b2bc49ba7
shima faghihiara <shimafaghihiara@gmail.com>
2021-08-04 16:48:50 +0430

    data fetxhing with swr

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,5 +1,7 @@
 import React,{useEffect,useState} from 'react';
 import axios from 'axios';
+import useSwr from "swr";
+import CircularProgress from "@material-ui/core/CircularProgress"
 
 
 const PostDetail = ({post}) => {
@@ -7,17 +9,19 @@ const PostDetail = ({post}) => {
 
     const [comments,setComments]=useState([]);
 
-//in hook useEffect raveshe csr ast
-    useEffect(()=>{
-        axios.get("http://localhost:3000/comments?postId0="+post.id)
-            .then(res=>{
-                console.log(res);
-                setComments(res.data);
-                }).catch(err=>{
-                    console.log(err);
-        });
+    const {data, error}=useSwr("http://localhost:3000/comments?postId0="+post.id,axios)
 
-    },[]);
+//in hook useEffect raveshe csr ast
+//     useEffect(()=>{
+//         axios.get("http://localhost:3000/comments?postId0="+post.id)
+//             .then(res=>{
+//                 console.log(res);
+//                 setComments(res.data);
+//                 }).catch(err=>{
+//                     console.log(err);
+//         });
+//
+//     },[]);
 
 
     return (
@@ -25,11 +29,13 @@ const PostDetail = ({post}) => {
             <h1>{post.title}</h1>
             <h2>{post.desc}</h2>
           <hr/>
-            <ul>
-                {
-                    comments.map((item, index)=><li key={index}><h3>{item.name}</h3><p>{item.descComment}</p></li>)
-                }
-            </ul>
+            {error && <h3>مشکل در لود صفحه</h3>}
+            { !data && <CircularProgress></CircularProgress>}
+
+            { data && <ul>
+                {data.data.map((item, index)=><li key={index}><h3>{item.name}</h3><p>{item.descComment}</p></li>)}
+                </ul>  }
+
         </div>
     );
 };
