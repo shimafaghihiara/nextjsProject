d401029c840c582e3d714f0bdb48fe705aec8f0b
shima faghihiara <shimafaghihiara@gmail.com>
2021-08-04 12:55:30 +0430

    talfigh csr and static generation

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,13 +1,35 @@
-import React from 'react';
-import axios from 'axios'
+import React,{useEffect,useState} from 'react';
+import axios from 'axios';
+
 
 const PostDetail = ({post}) => {
     console.log(post);
+
+    const [comments,setComments]=useState([]);
+
+//in hook useEffect raveshe csr ast
+    useEffect(()=>{
+        axios.get("http://localhost:3000/comments?postId0="+post.id)
+            .then(res=>{
+                console.log(res);
+                setComments(res.data);
+                }).catch(err=>{
+                    console.log(err);
+        });
+
+    },[]);
+
+
     return (
         <div>
             <h1>{post.title}</h1>
             <h2>{post.desc}</h2>
-            every things
+          <hr/>
+            <ul>
+                {
+                    comments.map((item, index)=><li key={index}><h3>{item.name}</h3><p>{item.descComment}</p></li>)
+                }
+            </ul>
         </div>
     );
 };
